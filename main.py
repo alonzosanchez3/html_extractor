b7008258085dc5d02f04b92f435e0bd87d409152
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-02-06 12:30:12 -0600

    implemented request

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open('./website.html', 'r') as html_file:
-  contents = html_file.read()
+response = requests.get('https://news.ycombinator.com/news')
 
-soup = BeautifulSoup(contents, 'html.parser')
+yc_web_page = response.text
 
-print(soup.title.string)
+soup = BeautifulSoup(yc_web_page, 'html.parser')
+print(soup.title)
+
+
+# with open('./website.html', 'r') as html_file:
+#   contents = html_file.read()
+
+# soup = BeautifulSoup(contents, 'html.parser')
+
+# all_anchor_tags = soup.find_all(name='a')
+
+# for tag in all_anchor_tags:
+#   print(tag.get('href'))
+
+# heading = soup.find(name='h1', id='name')
+# print(heading)
+
+# section_heading = soup.find(name='h3', class_='heading')
+# print(section_heading.text)
+
+# company_url = soup.select_one(selector='p a')
+# print(company_url.get('href'))
