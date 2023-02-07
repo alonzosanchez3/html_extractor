618e99832aa847dc03eab107cd0afa2f17157cef
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-02-07 12:30:12 -0600

    implemented feature to get most upvoted title and links

--- main.py
+++ main.py
@@ -6,13 +6,23 @@ response = requests.get('https://news.ycombinator.com/news')
 yc_web_page = response.text
 
 soup = BeautifulSoup(yc_web_page, 'html.parser')
-articles = soup.select(".titleline a")
+articles = soup.find_all(name='span', class_='titleline')
+print(len(articles))
 article_texts = [article.getText() for article in articles]
-article_links = [article.get('href') for article in articles]
-print(article_links)
+print(len(article_texts))
+article_links = [article.find(name='a').get('href') for article in articles]
 article_upvotes = soup.find_all(name='span', class_="score")
-article_upvotes = [article.getText() for article in article_upvotes]
+article_upvotes = [int(article.getText().split(' ')[0]) for article in article_upvotes]
 print(article_upvotes)
+
+max_index = 0
+for i in range(0, len(article_upvotes)):
+  if article_upvotes[max_index] < article_upvotes[i]:
+    max_index = i
+
+print(article_texts[max_index], article_links[max_index])
+
+
 
 
 
